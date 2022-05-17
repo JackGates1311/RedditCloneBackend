c437537daec3ded2d9317a9dbebb5014c1c5fa1e
Marko <makimitrovic07@gmail.com>
2022-05-18 00:11:36 +0200

    Initial commit

---
+++ src/main/java/com/example/sr2_2020/svt2021/projekat/Sr22020Svt2021ProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.example.sr2_2020.svt2021.projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sr22020Svt2021ProjekatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sr22020Svt2021ProjekatApplication.class, args);
+    }
+
+}
